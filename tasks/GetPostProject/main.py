2e7965d1eea4113e1b173d30232444284ed20f8d
Nikita Morkva <torbares@gmail.com>
2026-04-21 21:27:21 +0300

    create first CRUD request

--- tasks/GetPostProject/main.py
+++ tasks/GetPostProject/main.py
@@ -51,8 +51,31 @@ async def status_deliver(order_id:int) -> Dict:
             return Order(**order)
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='order not found ')
 
+@app.put('/orders/{order_id}/update-amount')
+async def update_amount(order_id:int, new_price: float , new_quantity: int):
+    for order in orders_db:
+        if order['id'] == order_id:
+            order['price'] = new_price
+            order['quantity'] = new_quantity
+            new_total = order['price'] * order['quantity']
+            if new_total > 5000:
+                order['priority'] = 'High'
+            else:
+                order['priority'] = 'Low'
+            return Order(**order)
+    raise HTTPException(status_code=404, detail="Orders not found")
 
+@app.delete('/orders/{order_id}/cancel')
+async def delete_order(order_id: int):
+    global orders_db
 
+    found = False
+    for order in orders_db:
+        if order['id'] == order_id:
+            found = True
+            break
+    if not found:
+        raise HTTPException(status_code=404, detail="Orders not found")
 
-
-
+    orders_db = [order for order in orders_db if order['id'] != order_id]
+    return {'data': f'order {order_id} has been cancelled'}
